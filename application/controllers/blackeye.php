b04823a55f32365c68ac4c139859c303e40c6aa4
sadball4 <62800320+sadball4@users.noreply.github.com>
2021-12-14 22:58:56 +0700

    Insert_data_base

--- application/controllers/blackeye.php
+++ application/controllers/blackeye.php
@@ -18,9 +18,33 @@ class Blackeye extends CI_Controller { //การตั้งชื่อ class
 	 * map to /index.php/welcome/<method_name>
 	 * @see https://codeigniter.com/user_guide/general/urls.html
 	 */
+    public function __construct()
+    {
+            parent::__construct();
+            // Your own constructor code
+            $this->name = 'Test'; // data main
+
+            $this->load->model('bass_model','bass');
+    }
 	public function index()
 	{
 		// $this->load->view('welcome_message');
         echo "BlackEye";
 	}
+    public function add(){
+        $this->load->view('bass_add_view');
+    }
+
+    public function save(){
+       // var_dump($_POST);
+        //$this->input->post('xxx') check scipt
+        $this->bass->save();
+
+        //$this->index();
+        redirect('bass');
+
+    }
+    public function detail(){
+        echo $this->name;
+    }
 }
