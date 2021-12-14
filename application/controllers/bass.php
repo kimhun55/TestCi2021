4504395f1584109eb63cdf73748c29e81546ebe9
icwebbas <93902185+icwebbas@users.noreply.github.com>
2021-12-14 22:05:45 +0700

    55

--- application/controllers/bass.php
+++ application/controllers/bass.php
@@ -3,25 +3,24 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Bass extends CI_Controller {
 
-	/**
-	 * Index Page for this controller.
-	 *
-	 * Maps to the following URL
-	 * 		http://example.com/index.php/welcome
-	 *	- or -
-	 * 		http://example.com/index.php/welcome/index
-	 *	- or -
-	 * Since this controller is set as the default controller in
-	 * config/routes.php, it's displayed at http://example.com/
-	 *
-	 * So any other public methods not prefixed with an underscore will
-	 * map to /index.php/welcome/<method_name>
-	 * @see https://codeigniter.com/user_guide/general/urls.html
-	 */
+   
+    public function __construct()
+    {
+            parent::__construct();
+            // Your own constructor code
+            $this->name = 'Test'; // data main
+    }
 	public function index()
 	{
-		// $this->load->view('welcome_message');
-        echo "bass";
-        echo "lnw";
+        
+
+        $data['name'] = "bass"; //main -> data
+        $data['data'] = array(1,2,3,4);
+		$this->load->view('bass_view',$data);
+        // view path view/
+       
 	}
+    public function detail(){
+        echo $this->name;
+    }
 }
